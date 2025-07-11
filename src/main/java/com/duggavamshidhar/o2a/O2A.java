084d5186eaa007ba1bc42547e7aa7c1971f49544
Dugga Vamshidhar Reddy <207072251+duggavamshidhar@users.noreply.github.com>
2025-07-11 17:10:12 +0530

    feat: init

---
+++ src/main/java/com/duggavamshidhar/o2a/O2A.java
@@ -0,0 +1,13 @@
+package com.duggavamshidhar.o2a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class O2A {
+
+    public static void main(String[] args) {
+        SpringApplication.run(O2A.class, args);
+    }
+
+}
---
+++ src/test/java/com/duggavamshidhar/o2a/O2ATests.java
@@ -0,0 +1,13 @@
+package com.duggavamshidhar.o2a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class O2ATests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
